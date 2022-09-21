0a85c46e0cc50fb0a3e130faf3e7912191f384b0
Ixhaaq <55302887+Ixhaaq@users.noreply.github.com>
2022-09-21 02:50:01 +0100

    Create Date Module, Refactor in app.js and date.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 
 app.set("view engine", "ejs");
@@ -16,16 +17,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-UK", options);
+    const day = date.getDate();
 
     res.render('list', {
         listTitle: day,
@@ -36,7 +28,7 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
 
     if (req.body.list === "Work") {
@@ -66,7 +58,7 @@ app.get("/about", function(req, res){
 })
 
 // app.post("/work", function(req, res){
-//     let item = req.body.newItem;
+//     const item = req.body.newItem;
 //     workItems.push(item);
 //     res.redirect("/work");
 
